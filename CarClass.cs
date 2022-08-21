67c7927f90a4d6d9ce88edbba563cff2a67fe834
DeletorDev <cesanchep@gmail.com>
2022-08-20 22:22:43 -0600

    Readme

--- CarClass.cs
+++ CarClass.cs
@@ -22,13 +22,12 @@ namespace CarAppV2
             Add(new Car() { Make = "Pontiac", Model = "FireBird", Year = 1967, EngineSize = 400 });
             Add(new Car() { Make = "Dodge", Model = "Challenger", Year = 1970, EngineSize = 427 });
             Add(new Car() { Make = "Ford", Model = "Pinto", Year = 1976, EngineSize = 208 });
-            Add(new Car() { Make = "AlfaRomeo", Model = "Tudeli", Year = 2000, EngineSize = 200 });
-            Add(new Car() { Make = "Porche", Model = "McPorcherson", Year = 2015, EngineSize = 450 });
+            Add(new Car() { Make = "AlfaRomeo", Model = "Tudela", Year = 2000, EngineSize = 200 });
+            Add(new Car() { Make = "Porche", Model = "Corcheros", Year = 2015, EngineSize = 296 });
             Add(new Car() { Make = "Pontiac", Model = "Trans Am", Year = 1976, EngineSize = 455 });
             Add(new Car() { Make = "Chevy", Model = "Camaro", Year = 2022, EngineSize = 476 });
             Add(new Car() { Make = "Ford", Model = "Bronco", Year = 1985, EngineSize = 350 });
             Add(new Car() { Make = "Chevy", Model = "Cavalier", Year = 1996, EngineSize = 287 });
-            Add(new Car() { Make = "Porche", Model = "911", Year = 1999, EngineSize = 327 });
         }
     }
 }
